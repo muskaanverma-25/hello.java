8bf6563b75e616551a7c290c37de22c4db9af015
muskaanverma-25 <80263092+muskaanverma-25@users.noreply.github.com>
2021-04-21 15:56:36 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-public class hello {
-public static void main(String[] args)
+public class Main
 {
-System.out.println("hey from github");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
